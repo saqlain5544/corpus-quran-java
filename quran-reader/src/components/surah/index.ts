02462b6f85062a06b317ed80d940bc8bd6c04473
Saqlain Ishtiaq <saqlainishtiaq@gmail.com>
2026-06-25 18:48:08 +1000

    chore(git): untrack deleted quran-reader/ codebase and extend .gitignore
    The Next.js reader app was deleted from disk but its files remained in
    the index. Untrack them with 'git rm -r --cached quran-reader/'.
    Also extend .gitignore to keep generated/regenerable artefacts out of
    the repo (bin/quranreader, data/processed/*.gob, data/new/*.db, etc.).

--- quran-reader/src/components/surah/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Barrel export for the surah reader sub-components.
- * Import from "@/components/surah" to get all sub-components.
- */
-
-export { SurahHeader } from "./SurahHeader";
-export { FooterNav } from "./FooterNav";
-export { VerseView } from "./VerseView";
